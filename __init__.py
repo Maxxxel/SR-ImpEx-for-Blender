5b89cf0f3a3b397c07e6f2a83ba26aca68122126
Maxxxel <Maxxxel@users.noreply.github.com>
2023-04-29 14:20:52 +0200

    Increase version numbering

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
 	"author" : "Maxxxel",
 	"description" : "Addon for importing and exporting Battleforge drs/bmg files",
 	"blender" : (3, 4, 0),
-	"version" : (0, 0, 1),
+	"version" : (0, 0, 2),
 	"location" : "File > Import",
 	"warning" : "",
 	"category" : "Import-Export",
