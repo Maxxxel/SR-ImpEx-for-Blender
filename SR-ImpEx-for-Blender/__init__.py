9a18e86025af968e2ee8a73e47471d2f5b319f75
Maxxxel <m.gromball@gmx.de>
2024-11-24 02:49:40 +0100

    version increase

--- SR-ImpEx-for-Blender/__init__.py
+++ SR-ImpEx-for-Blender/__init__.py
@@ -26,7 +26,7 @@ bl_info = {
 	"author" : "Maxxxel",
 	"description" : "Addon for importing and exporting Battleforge drs/bmg files",
 	"blender" : (4, 0, 0),
-	"version" : (2, 4, 5),
+	"version" : (2, 5, 0),
 	"location" : "File > Import",
 	"warning" : "",
 	"category" : "Import-Export",
