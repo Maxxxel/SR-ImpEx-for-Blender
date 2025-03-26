4d4140ab6d776279e0ad3fbcda68de00ffbb83a0
Maxxxel <Maxxxel@users.noreply.github.com>
2025-03-26 13:41:12 +0100

    Update addon_updater_ops.py
    Fixed Auto-Update

--- sr_impex/addon_updater_ops.py
+++ sr_impex/addon_updater_ops.py
@@ -1367,7 +1367,7 @@ def register(bl_info):
     # Addon subfolder path.
     # "sample/path/to/addon"
     # default is "" or None, meaning root
-    updater.subfolder_path = ""
+    updater.subfolder_path = "sr_impex"
 
     # Used to check/compare versions.
     updater.current_version = bl_info["version"]
