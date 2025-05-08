9869a940f90df8949b4a6dfb1a87dcfc3e72348b
Maxxxel <m.gromball@gmx.de>
2025-05-08 12:46:07 +0200

    Update version number to 2.3.0 in addon metadata

--- sr_impex/__init__.py
+++ sr_impex/__init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "author": "Maxxxel",
     "description": "Addon for importing and exporting Battleforge drs/bmg files.",
     "blender": (4, 3, 0),
-    "version": (2, 9, 3),
+    "version": (2, 3, 0),
     "location": "File > Import",
     "warning": "",
     "category": "Import-Export",
