a872b77661c5c81df4d7ed0319c02f6ea8e8eb4f
Maxxxel <m.gromball@gmx.de>
2025-10-29 13:09:15 +0100

    Updated: Version and Release Package

--- sr_impex/__init__.py
+++ sr_impex/__init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "author": "Maxxxel",
     "description": "Addon for importing and exporting Battleforge drs/bmg files.",
     "blender": (4, 5, 0),
-    "version": (3, 4, 0),
+    "version": (3, 5, 0),
     "location": "File > Import",
     "warning": "",
     "category": "Import-Export",
