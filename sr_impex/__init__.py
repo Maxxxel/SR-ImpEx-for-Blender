bda03419e4d9f1d8f73189d245478199cc63bd8f
Maxxxel <m.gromball@gmx.de>
2025-11-06 11:56:27 +0100

    Hotfix: missed an argument for BMG Import,

--- sr_impex/__init__.py
+++ sr_impex/__init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "author": "Maxxxel",
     "description": "Addon for importing and exporting Battleforge drs/bmg files.",
     "blender": (4, 5, 0),
-    "version": (3, 6, 0),
+    "version": (3, 6, 1),
     "location": "File > Import",
     "warning": "",
     "category": "Import-Export",
